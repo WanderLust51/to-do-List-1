d31d3a9a8a76d58291a8876fee66e1d1ec6b773c
Andrés Lugo <andreslugo51@gmail.com>
2023-06-10 18:46:18 -0400

    Medianamente funcional

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
-let todo = '';
 let todoList = [];
-let listSize;
 
 $(document).ready(function(){
 
@@ -11,20 +9,74 @@ $(document).ready(function(){
 
 function addlist(){
     todo = $('#toDoInput').val();
+    $('#toDoInput').val('');
 
-    listSize = todoList.length;
     todoList.push(todo);
+    let id = todoList.length - 1;
 
-    addRow(todo);
+    addRow(todo, id);
 }
 
-function addRow(todo){
+function addRow(todo, id){
 
-    newRow = ''
+    let newRow = document.createElement('div');
+    $(newRow).addClass('todo').addClass('my-bg-green3');
+    $(newRow).attr('id', id)
+    
+    let div1 = document.createElement('div');
+    let h4 = document.createElement('h4');
+    $(h4).text(todo);
+    $(div1).append(h4);
+    
+    let div2 = document.createElement('div');
+    let checkDiv = document.createElement('div');
+    let cancelDiv = document.createElement('div');
+    let check = document.createElement('ion-icon');
+    let cancel = document.createElement('ion-icon');
+    $(check).attr('name', 'checkmark-outline');
+    $(cancel).attr('name', 'close-outline');
+    $(checkDiv).append(check).addClass('my-bg-hover');
+    $(cancelDiv).append(cancel).addClass('my-bg-hover');
 
+    $(checkDiv).click(completeTodo).click(deleteRow);
+    $(cancelDiv).click(deleteRow);
 
-    $('#list').append();
+    $(div2).append(checkDiv).append(cancelDiv);
+
+    $(newRow).append(div1);
+    $(newRow).append(div2);
+
+    $('#list').append(newRow);
 }
+
+function deleteRow(){
+    let row = $(this).parent().parent();
+    let id = Number($(row).attr('id'));
+    console.log(`Eliminando Nº ${id+1}.`)
+
+    //ELIMINANDO DEL ARREGLO
+    todoList.splice(id,1);
+
+    //ELIMINANDO EL ELEMENTO
+    $(row).remove();
+
+    sortRows();
+    updateStorage();
+}
+function completeTodo(){
+    console.log('COMPLETADO')
+}
+
+function sortRows(){
+    let list = $('#list').children();
+
+    for (i = 0; i < list.length; i++){
+        let div = list[i];
+
+        $(div).attr('id', i);
+    }
+}
+
 
 function updateStorage(){
     let list = JSON.stringify(todoList);
